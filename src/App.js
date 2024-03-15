1eff46873dc7e5077b12fe77a40e648b98b8548b
Khoa Hoang <khoahoang1698@gmail.com>
2024-03-15 16:16:59 -0700

    done basic navbar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@
 function App() {
     return (
         <div className="App">
-
+            <img src="https://smarternutrition.com/cdn/shop/products/smarter-curcumin-banner-d.04.png?v=1644620182&width=1500" alt="" />
+            <img src="https://smarternutrition.com/cdn/shop/products/smarter-curcumin-banner-d.04.png?v=1644620182&width=1500" alt="" />
+            
+            <img src="https://smarternutrition.com/cdn/shop/products/smarter-curcumin-banner-d.04.png?v=1644620182&width=1500" alt="" />
         </div>
     );
 }
